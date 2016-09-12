4e4ce432534510ab0c8650b0c95efdd83b610319
toransahu <toran.sahu@yahoo.com>
2016-09-12 18:54:33 +0530

    coded little

--- queue/aStackUsingSingleQueue.py
+++ queue/aStackUsingSingleQueue.py
@@ -1,8 +1,18 @@
 from collections import deque
-q=deque()
-q.append('c')
-q.append('d')
 
-print(q.popleft())
-print(q)
+class StackFromQueue:
+    q=deque()
+    def __init__(self):
+        self.q.pop()
+    def enqueue(self,val):
+        self.q.append(val)
+        return True
+    def deque(self):
+        top=self.q.popfromleft()
+        return top()
+sfq=StackFromQueue()
+sfq.enqueue('a')
+print(sfq)
+ 
+        
 
