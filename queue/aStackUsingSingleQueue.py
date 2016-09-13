57d447c6dda68997978b8e27b9fae669751295b5
toransahu <toran.sahu@yahoo.com>
2016-09-13 18:57:58 +0530

    added queuelib module

--- queue/aStackUsingSingleQueue.py
+++ queue/aStackUsingSingleQueue.py
@@ -1,17 +1,17 @@
-
-from collections import deque
-
+import queuelib
+from queuelib.queue import FifoDiskQueue
 class StackFromQueue:
-    q=deque()
+    
+    q = queuelib.queue.FifoDiskQueue("D:",100)    
     def __init__(self):
-        self.q.pop()
+        #self.q.pop()
+        pass
     def enqueue(self,val):
-        self.q.append(val)
-        return True
-    def deque(self):
-        top=self.q.popfromleft()
-        return top()
-sfq=StackFromQueue()
-sfq.enqueue('a')
-print(sfq)
- 
+        self.q.push(val)
+    def dequeue(self):
+        first=self.q.pop()
+        return first
+    
+q1=StackFromQueue()
+q1.enqueue()
+print(q1.dequeue())
