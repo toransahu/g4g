fa3e862aa0037766f9ee0e22403c0d1d417f5600
TORAN <toran.sahu@yahoo.com>
2018-01-03 00:02:18 +0530

    package created

---
+++ ds/test-tree/test_binary_tree_pytest0.py
@@ -0,0 +1,24 @@
+from .binary_tree import in_order, Node
+
+
+def test_in_order_positive():
+    """
+    Testing inorder traversal.
+    """
+    root = Node(1)
+    root.left = Node(2)
+    root.right = Node(3)
+    root.left.left = Node(4)
+    root.left.right = Node(5)
+    result = []
+    assert in_order(root, result) == [4, 2, 5, 1, 3]
+
+
+def test_in_order_negative():
+    root = Node(1)
+    root.left = Node(2)
+    root.right = Node(3)
+    root.left.left = Node(4)
+    root.left.right = Node(5)
+    result = []
+    assert in_order(root, result) != []
---
+++ ds/test-tree/test_binary_tree_unittest.py
@@ -0,0 +1,29 @@
+import unittest
+try:
+    from .binary_tree import in_order, Node
+except:
+    from binary_tree import in_order, Node
+
+
+class TestBinaryTree(unittest.TestCase):
+    # override inbuit method setUp, to setup resources
+    def setUp(self):
+        self.root = Node(1)
+        self.root.left = Node(2)
+        self.root.right = Node(3)
+        self.root.left.left = Node(4)
+        self.root.left.right = Node(5)
+        self.result = []
+        self.func_in_order = in_order(self.root, self.result)
+
+    def test_in_order_positive(self):
+        self.assertEqual(self.func_in_order, [4, 2, 5, 1, 3], msg=None)
+
+
+    def test_in_order_negative(self):
+        self.assertNotEqual(self.func_in_order, [], msg=None)
+
+
+if __name__ == '__main__':
+    root = None
+    unittest.main()
