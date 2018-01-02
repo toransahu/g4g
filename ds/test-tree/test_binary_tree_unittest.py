67b4cbf19044a6860f4d8e44ab58410055fb2183
TORAN <toran.sahu@yahoo.com>
2018-01-03 00:06:00 +0530

    unittest + class based + setUp

--- ds/test-tree/test_binary_tree_unittest.py
+++ ds/test-tree/test_binary_tree_unittest.py
@@ -1,12 +1,12 @@
 import unittest
 try:
-    from .binary_tree import in_order, Node
+    from ..binary_tree import in_order, Node
 except:
-    from binary_tree import in_order, Node
+    from ds.tree.binary_tree import in_order, Node
 
 
 class TestBinaryTree(unittest.TestCase):
-    # override inbuit method setUp, to setup resources
+    # override in-built method setUp, to setup resources
     def setUp(self):
         self.root = Node(1)
         self.root.left = Node(2)
@@ -18,7 +18,6 @@ class TestBinaryTree(unittest.TestCase):
 
     def test_in_order_positive(self):
         self.assertEqual(self.func_in_order, [4, 2, 5, 1, 3], msg=None)
-
 
     def test_in_order_negative(self):
         self.assertNotEqual(self.func_in_order, [], msg=None)
