9548e3ed017c45bbbbaf6623779a72426faf7045
TORAN <toran.sahu@yahoo.com>
2018-01-04 23:46:41 +0530

    level_order_traversal, level_of_tree

--- ds/tree/binary_tree.py
+++ ds/tree/binary_tree.py
@@ -66,12 +66,51 @@ def post_order(root, result):
     return result
 
 
+def level_order_traversal(root):
+    """
+    Traverse in Breadth First Search order.
+
+    :param root: Root of the tree
+    :param result: Result in list format
+    :return: result
+    """
+    # find level of the tree
+    l = level_of_tree(root)
+
+    # for exclusive upper bound use l + 1
+    for level in range(1, l+1):
+        print_given_level(root, level)
+
+
+def level_of_tree(root):
+    if root:
+        l = level_of_tree(root.left)
+        r = level_of_tree(root.right)
+        if l > r:
+            return l + 1
+        else:
+            return r + 1
+    else:
+        return 0
+
+
+def print_given_level(root, level):
+    if root is None:
+        return
+    elif level == 1:
+        print(root.data)
+    elif level > 1:
+        print_given_level(root.left, level - 1)
+        print_given_level(root.right, level - 1)
+
+
 root = Node(1)
 root.left = Node(2)
 root.right = Node(3)
 root.left.left = Node(4)
 root.left.right = Node(5)
-
-print(in_order(root, []))
-print(pre_order(root, []))
-print(post_order(root, []))
+# print(in_order(root, []))
+# print(pre_order(root, []))
+# print(post_order(root, []))
+# level_order_traversal(root)
+print(level_of_tree(root))
