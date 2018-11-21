cc83d6ab60b6196f7ed7d570216df2f7202fd07b
Toran Sahu <toran.sahu@yahoo.com>
2018-11-21 15:39:44 +0530

    [added] dp problems

---
+++ algo/dp/edit_distance.py
@@ -0,0 +1,60 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+# created_on: 2018-11-21 12:47
+
+"""
+edit_distance.py
+"""
+
+
+__author__ = "Toran Sahu <toran.sahu@yahoo.com>"
+__license__ = "Distributed under terms of the MIT license"
+
+
+class Algorithm:
+    """Edit Distance Algorithm Class."""
+
+    def __init__(self, str1, str2):
+        """Initialize the Class.
+
+        :param str1: String A
+        :param str2: String B
+        """
+        self._str1 = str1
+        self._str2 = str2
+        self._len1 = len(self._str1)
+        self._len2 = len(self._str2)
+        self._distance_table = [
+            [0 for _ in range(self._len1 + 1)] for _ in range(self._len2 + 1)
+        ]
+
+    def _compute(self):
+        """Compute the output."""
+        if self._len1 == 0:
+            return self._len2
+        elif self._len2 == 0:
+            return self._len1
+
+        for i in range(1, self._len1):
+            for j in range(1, self._len2):
+                if self._str1[i - 1 - 1] == self._str2[j - 1 - 1]:
+                    self._distance_table[i][j] = self._distance_table[i - 1][
+                        j - 1
+                    ]
+                else:
+                    self._distance_table[i][j] = 1 + min(
+                        self._distance_table[i - 1][j - 1],
+                        self._distance_table[i - 1][j],
+                        self._distance_table[i][j - 1],
+                    )
+        return self._distance_table[i][j]
+
+
+def edit_distance(string_a, string_b):
+    """Compute Edit Distance.
+
+    :param string_a: String A
+    :param string_b: String B
+    """
+    return Algorithm(string_a, string_b)._compute()
