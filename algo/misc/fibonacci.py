e183229e0bc8b6db0b6b5ebbe2f761f97ec02d3e
Toran Sahu <toran.sahu@yahoo.com>
2018-11-22 19:52:55 +0530

    [added] fibonacci algo

---
+++ algo/misc/fibonacci.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+# created_on: 2018-11-16 00:23
+
+"""
+fibonacci.py
+"""
+
+
+__author__ = "Toran Sahu <toran.sahu@yahoo.com>"
+__license__ = "Distributed under terms of the MIT license"
+
+
+def fib_recursive(n):
+    pass
