92111a939e6c4d205dfeef01660eda6f95210ed3
Toran Sahu <toran.sahu@yahoo.com>
2018-11-26 17:46:25 +0530

    [initial] longest common substring sol

---
+++ algo/dp/longest_common_substring.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+# created_on: 2018-11-26 17:08
+
+"""Longest Common Substring."""
+
+
+from pprint import pprint
+
+
+__author__ = "Toran Sahu <toran.sahu@yahoo.com>"
+__license__ = "Distributed under terms of the MIT license"
+
+
+class Algorithm:
+    """Algorithm."""
+
+    def __init__(self, str_a, str_b):
+        """Initialize.
+
+        :param str_a: String <input string a>
+        :param str_b: String <input string b>
+        """
+        self._str_a = str_a
+        self._str_b = str_b
+
+    def _compute(self):
+        """Compute LCS."""
+        pass
