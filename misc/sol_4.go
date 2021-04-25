aeae8881b5217d5ec25c8b1cc6eb7aa3c15cd756
Toran Sahu <toran.sahu@yahoo.com>
2021-04-25 17:40:35 +0530

    solution to misc problem 4

---
+++ misc/sol_4.go
@@ -0,0 +1,46 @@
+/*
+sol_4.go
+Copyright (C) 2021 Toran Sahu <toran.sahu@yahoo.com>
+
+Distributed under terms of the MIT license.
+*/
+
+// Package misc
+package misc
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/toransahu/goutils/adt"
+)
+
+func Sol4(input []string) []int {
+	firstLine := input[0]
+	remainingLines := input[1:]
+
+	numOfTasks, err := strconv.Atoi(firstLine)
+	if err != nil {
+		panic(err)
+	}
+
+	dependencies := [][]int{}
+	for _, line := range remainingLines {
+		dep := strings.Split(line, "<")
+		u, err := strconv.Atoi(dep[0])
+		if err != nil {
+			panic(err)
+		}
+		v, err := strconv.Atoi(dep[1])
+		if err != nil {
+			panic(err)
+		}
+		dependencies = append(dependencies, []int{u, v})
+	}
+
+	g := adt.NewGraph(numOfTasks)
+	for _, dep := range dependencies {
+		g.AddEdge(dep[0], dep[1])
+	}
+	return g.TopoSort()
+}
---
+++ misc/sol_4_test.go
@@ -0,0 +1,40 @@
+/*
+sol_4_test.go
+Copyright (C) 2021 Toran Sahu <toran.sahu@yahoo.com>
+
+Distributed under terms of the MIT license.
+*/
+
+// Package misc
+package misc
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSol4(t *testing.T) {
+	testcases := []struct {
+		given []string
+		want  []int
+	}{
+		{
+			given: []string{"5", "0<3", "3<4", "4<1", "2<1", "2<0"},
+			want:  []int{2, 0, 3, 4, 1},
+		},
+		{
+			given: []string{"5", "0<3", "2<1", "3<4", "2<0", "2<3", "0<4", "0<1"},
+			want:  []int{2, 0, 1, 3, 4},
+		},
+	}
+
+	for _, testcase := range testcases {
+
+		input := testcase.given
+		want := testcase.want
+		result := Sol4(input)
+		if !reflect.DeepEqual(result, want) {
+			t.Errorf("for input: %v , want: %v, got: %v", input, want, result)
+		}
+	}
+}
