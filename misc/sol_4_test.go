10b83a03aa54e532e43010c89c3b3e40ce187acf
Toran Sahu <toran.sahu@yahoo.com>
2021-04-25 20:12:46 +0530

    one more test case

--- misc/sol_4_test.go
+++ misc/sol_4_test.go
@@ -26,6 +26,10 @@ func TestSol4(t *testing.T) {
 			given: []string{"5", "0<3", "2<1", "3<4", "2<0", "2<3", "0<4", "0<1"},
 			want:  []int{2, 0, 1, 3, 4},
 		},
+		{
+			given: []string{"6", "2<3", "3<1", "4<0", "4<1", "5<2", "5<0"},
+			want:  []int{5, 4, 2, 3, 1, 0},
+		},
 	}
 
 	for _, testcase := range testcases {
