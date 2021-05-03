af4835d2c3b31a5ef17a98da0cefe6606755f80e
Toran Sahu <toran.sahu@yahoo.com>
2021-05-03 12:08:36 +0530

    refactor(solve p6): merge K sorted linkedlist using minheap/priority queue

--- misc/sol_6.go
+++ misc/sol_6.go
@@ -66,7 +66,7 @@ func Sol_6_v1(input [][]int) []int {
 				break
 			}
 			// insert the head to sorted/result linkedlist
-			insertInSortedOrder(&resultLL, head) // O(K)? or O(K*N)? or (K * logN)?
+			InsertInSortedOrder(&resultLL, head) // O(K)? or O(K*N)? or (K * logN)?
 		}
 	}
 
@@ -80,7 +80,9 @@ func Sol_6_v1(input [][]int) []int {
 	return res
 }
 
-func insertInSortedOrder(l *adt.SLinkedlist, node *adt.SLLNode) {
+// InsertInSortedOrder inserts the given node in the given linkedlist such that the linkedlist is always maintained in a ASC sorted order
+// A kind of insert to a priority queue implemention (reversed order)
+func InsertInSortedOrder(l *adt.SLinkedlist, node *adt.SLLNode) {
 	if l.IsEmpty() {
 		l.Head = node
 		node.Next = nil
@@ -121,6 +123,100 @@ func Sol_6_v2(input [][]int) {}
 // Approach: delete the heads of each linkedlists and create a Min-Heap (priority queue), then pick the min element from the heap and append to the result linkedlist, replace that min element in the heap with with its next node in its linkedlist
 // Time complexity: O(K*N)
 // Auxilary space: O(K)
-func Sol_6_v3(input [][]int) {
+func Sol_6_v3(input [][]int) []int {
+	// K - number of linkedlists
+	k := len(input)
+	// a map to hold K linkedlists
+	linkedLists := make(map[int]*adt.SLinkedlist, k) // max size would be K
+	// iterate/parse the input matrix row by row
+	for i, list := range input {
+		// create a new linkedlist
+		l := adt.NewSLinkedList()
+		// iterate through each item in the row
+		for _, item := range list {
+			// add as a node to the linkedlist
+			l.Append(item)
+		}
+		// add this linkedlist to the  t(holder)
+		linkedLists[i] = &l
+	}
 
+	// (low) priority queue / min heap - of K nodes - using linkedlist
+	priorityQ := adt.NewSLinkedList()
+
+	// a map to which linkedlist this node belongs to
+	nodeToSLL := map[*adt.SLLNode]*adt.SLinkedlist{}
+
+	// iterate through each linkedlists
+	for _, l := range linkedLists {
+		// linkedlist could be empty - add a check
+		if l.IsEmpty() {
+			continue
+		}
+
+		// get the head
+		head := l.Head
+
+		// delete the head
+		err := l.Remove(l.Head)
+		if err != nil {
+			panic(err)
+		}
+
+		// keep map of - which linkedlist this node belongs to
+		nodeToSLL[head] = l
+
+		// insert the first K heads (of each linkedlists) to priority queue
+		InsertInSortedOrder(&priorityQ, head) // O(K)
+	}
+
+	// resultant linkedlist
+	resultLL := adt.NewSLinkedList()
+
+	// while all the linkedlist are NON Empty
+	for {
+		// if the priorityQ is empty, then we have merged all nodes
+		if priorityQ.IsEmpty() {
+			break
+		}
+
+		// get the min node
+		minNode := priorityQ.Head
+
+		// remove the min node from the priority queue
+		priorityQ.Remove(priorityQ.Head)
+
+		// append the min node to the result linkedlist
+		resultLL.AppendNode(minNode)
+
+		// get the belonging linkedlist
+		l := nodeToSLL[minNode]
+
+		// if the linkedlist is empty then update the `done` count
+		if l.IsEmpty() {
+			// increase the counter
+			continue
+		}
+		// get the next node from the same linkedlist
+		nextNode := l.Head
+
+		// remove the node from the linkedlist
+		l.Remove(l.Head)
+
+		// map the linkedlist corresponding to this next node
+		nodeToSLL[nextNode] = l
+
+		// append it to the priorityQ i.e.
+		// fill up/replace (append) the removed node in priority queue with its next node
+		InsertInSortedOrder(&priorityQ, nextNode)
+	}
+
+	// an array to return the sorted data
+	res := []int{}
+	// iterate through the result linkedlist
+	for n := range resultLL.Iterate() {
+		// append each node's data to the result array
+		res = append(res, n.Data.(int))
+	}
+	return res
 }
--- misc/sol_6_test.go
+++ misc/sol_6_test.go
@@ -16,12 +16,36 @@ import (
 	"github.com/toransahu/goutils/adt"
 )
 
+func TestSol6_v3(t *testing.T) {
+	testcases := []struct {
+		given  [][]int
+		wanted []int
+	}{
+		{given: [][]int{{1, 3, 7}, {2, 4, 6}, {0, 5, 8}}, wanted: []int{0, 1, 2, 3, 4, 5, 6, 7, 8}},
+		{given: [][]int{{1, 3, 7}}, wanted: []int{1, 3, 7}},
+		{given: [][]int{{1, 3, 7}, {}, {}}, wanted: []int{1, 3, 7}},
+		{given: [][]int{{1, 3, 7}, {0}, {}}, wanted: []int{0, 1, 3, 7}},
+		{given: [][]int{{1, 3, 7}, {0}, {-1}}, wanted: []int{-1, 0, 1, 3, 7}},
+	}
+
+	for _, testcase := range testcases {
+		got := Sol_6_v3(testcase.given)
+		if !reflect.DeepEqual(got, testcase.wanted) {
+			t.Errorf("given: %v, got: %v, want: %v", testcase.given, got, testcase.wanted)
+		}
+	}
+}
+
 func TestSol6_v1(t *testing.T) {
 	testcases := []struct {
 		given  [][]int
 		wanted []int
 	}{
 		{given: [][]int{{1, 3, 7}, {2, 4, 6}, {0, 5, 8}}, wanted: []int{0, 1, 2, 3, 4, 5, 6, 7, 8}},
+		{given: [][]int{{1, 3, 7}}, wanted: []int{1, 3, 7}},
+		{given: [][]int{{1, 3, 7}, {}, {}}, wanted: []int{1, 3, 7}},
+		{given: [][]int{{1, 3, 7}, {0}, {}}, wanted: []int{0, 1, 3, 7}},
+		{given: [][]int{{1, 3, 7}, {0}, {-1}}, wanted: []int{-1, 0, 1, 3, 7}},
 	}
 
 	for _, testcase := range testcases {
@@ -32,17 +56,17 @@ func TestSol6_v1(t *testing.T) {
 	}
 }
 
-func Test_insertInSortedOrder(t *testing.T) {
+func TestInsertInSortedOrder(t *testing.T) {
 	l := adt.NewSLinkedList()
-	insertInSortedOrder(&l, adt.NewSLLNode(1))
-	insertInSortedOrder(&l, adt.NewSLLNode(3))
-	insertInSortedOrder(&l, adt.NewSLLNode(7))
-	insertInSortedOrder(&l, adt.NewSLLNode(2))
-	insertInSortedOrder(&l, adt.NewSLLNode(4))
-	insertInSortedOrder(&l, adt.NewSLLNode(6))
-	insertInSortedOrder(&l, adt.NewSLLNode(0))
-	insertInSortedOrder(&l, adt.NewSLLNode(5))
-	insertInSortedOrder(&l, adt.NewSLLNode(8))
+	InsertInSortedOrder(&l, adt.NewSLLNode(1))
+	InsertInSortedOrder(&l, adt.NewSLLNode(3))
+	InsertInSortedOrder(&l, adt.NewSLLNode(7))
+	InsertInSortedOrder(&l, adt.NewSLLNode(2))
+	InsertInSortedOrder(&l, adt.NewSLLNode(4))
+	InsertInSortedOrder(&l, adt.NewSLLNode(6))
+	InsertInSortedOrder(&l, adt.NewSLLNode(0))
+	InsertInSortedOrder(&l, adt.NewSLLNode(5))
+	InsertInSortedOrder(&l, adt.NewSLLNode(8))
 
 	want := []int{0, 1, 2, 3, 4, 5, 6, 7, 8}
 	got := []int{}
