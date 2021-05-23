6eb0fdf3f9b056ce9b6b0c07d08fea53d9949fd1
Toran Sahu <toran.sahu@yahoo.com>
2021-05-23 19:40:00 +0530

    solution to misc problem 8 - v2

--- misc/sol_8.go
+++ misc/sol_8.go
@@ -10,6 +10,65 @@ package misc
 
 import "github.com/toransahu/goutils/adt"
 
+// Sol8_v2 solves the problem by using Floyd's cycle detection method.
+// Approach: maintain 2 pointers, 1 slow (1 jump at a iteration), 1 fast (2 jumps at a iteration) - if both the pointers points to the same node, then there is a loop
+// Idea:
+func Sol8_v2(l *adt.SLinkedlist) (bool, *adt.SLinkedlist) {
+	if l.IsEmpty() || l.Head.Next == nil {
+		return false, l
+	}
+	if l.Head.Next == l.Head {
+		l.Head.Next = nil
+		return true, l
+	}
+
+	pS := l.Head
+	pF := l.Head
+	var meetingPoint *adt.SLLNode
+
+	for {
+		if pF.Next == nil || pF.Next.Next == nil {
+			return false, l
+		}
+
+		if pS.Next == pF.Next.Next {
+			meetingPoint = pS.Next
+			break
+		}
+
+		pS = pS.Next
+		pF = pF.Next.Next
+	}
+
+	// if we're here, then we've detected a loop in the linkedlist
+
+	// check if the linkedlist is _circular_
+	if meetingPoint == l.Head {
+		// make it non-circular
+		pF.Next = nil
+		return true, l
+	}
+
+	// if we're here then the linkedlist a _loop_ and we need to find the start node of the loop
+
+	pS = l.Head
+	pF = meetingPoint
+
+	for {
+		if pS.Next == pF.Next {
+			break
+		}
+		pS = pS.Next
+		pF = pF.Next
+	}
+
+	pF.Next = nil
+	return true, l
+}
+
+// Sol8 solves the problem by hashing the visited node's address and matching the current node's address against the hash map.
+// TC: O(n)
+// SC: O(n)
 func Sol8(l *adt.SLinkedlist) bool {
 
 	if l.IsEmpty() {
--- misc/sol_8_test.go
+++ misc/sol_8_test.go
@@ -9,6 +9,7 @@ Distributed under terms of the MIT license.
 package misc
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -16,28 +17,86 @@ import (
 )
 
 func TestSol8(t *testing.T) {
-	l := adt.NewSLinkedList()
-	l.Append(1)
-	node2 := l.Append(2)
-	l.Append(3)
-	l.Append(4)
-	node5 := l.Append(5)
-	node5.Next = node2
-
 	/*
 			[1] → [2] → [3]
 		           ↑     ↓
 		          [5] ← [4]
 	*/
+	l1 := adt.NewSLinkedList()
+	l1.Append(1)
+	node2 := l1.Append(2)
+	l1.Append(3)
+	l1.Append(4)
+	node5 := l1.Append(5)
+	node5.Next = node2
 
-	assert.True(t, Sol8(&l))
+	/*
+			[1] → [2] → [3]
+		                 ↓
+		          [5] ← [4]
+	*/
+	l2 := adt.NewSLinkedList()
+	l2.Append(1)
+	l2.Append(2)
+	l2.Append(3)
+	l2.Append(4)
+	l2.Append(5)
 
-	l = adt.NewSLinkedList()
-	l.Append(1)
-	l.Append(2)
-	l.Append(3)
-	l.Append(4)
-	l.Append(5)
+	/*
+		<empty/nil>
+	*/
+	l3 := adt.NewSLinkedList()
 
-	assert.False(t, Sol8(&l))
+	/*
+		[1]
+	*/
+	l4 := adt.NewSLinkedList()
+	l4.Append(1)
+
+	/*
+		   [1]-->
+		    ↑   ↓
+			<----
+	*/
+	l5 := adt.NewSLinkedList()
+	l5.Append(1)
+	l5.Head.Next = l5.Head
+
+	/*
+			[1] → [2] → [3]
+		     ↑           ↓
+		    [6] ← [5] ← [4]
+	*/
+	l6 := adt.NewSLinkedList()
+	l6.Append(1)
+	l6.Append(2)
+	l6.Append(3)
+	l6.Append(4)
+	l6.Append(5)
+	node6 := l6.Append(6)
+	node6.Next = l6.Head
+
+	testcases := []struct {
+		given  *adt.SLinkedlist
+		wanted bool
+	}{
+		{given: &l1, wanted: true},
+		{given: &l2, wanted: false},
+		{&l3, false},
+		{&l4, false},
+		{&l5, true},
+		{&l6, true},
+	}
+
+	for _, tc := range testcases {
+		got := Sol8(tc.given)
+		assert.Equal(t, tc.wanted, got)
+
+		got, l := Sol8_v2(tc.given)
+		assert.Equal(t, tc.wanted, got)
+		for node := range l.Iterate() {
+			fmt.Println(node.Data)
+		}
+		fmt.Println()
+	}
 }
