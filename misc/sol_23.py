df2e403159fd05fcb7ba22dc14eaf414fc7ad618
Toran Sahu <toran.sahu@yahoo.com>
2021-09-07 17:54:40 +0530

    solution 23 v2

--- misc/sol_23.py
+++ misc/sol_23.py
@@ -10,6 +10,44 @@ __author__ = 'Toran Sahu <toran.sahu@yahoo.com>'
 __license__ = 'Distributed under terms of the MIT license'
 
 
+def sol_23_v2(arr, n):
+    """Solve is single iteration"""
+    if n <= 1:
+        return arr
+
+    # maintain 3 pointers
+    l = 0
+    r = n-1
+    m = 0
+
+    # while l & r does not colloid/cross each other and m also does not cross the r
+    while l < r and m < r:
+        # if element at l is not 0 then check r & m
+        l_solved = False
+        r_solved = False
+        if arr[l] != 0:
+            if arr[r] == 0:
+                arr[r], arr[l] = arr[l], arr[r]
+                l += 1
+                l_solved = True
+            elif arr[m] == 0:
+                arr[m], arr[l] = arr[l], arr[m]
+                l += 1
+                l_solved = True
+        if arr[r] != 2:
+            if arr[l] == 2:
+                arr[r], arr[l] = arr[l], arr[r]
+                r -= 1
+                r_solved = True
+            elif arr[m] == 2:
+                arr[m], arr[r] = arr[r], arr[m]
+                r -= 1
+                r_solved = True
+        if not any([l_solved, r_solved]):
+            m += 1
+    return arr
+
+
 def sol_23(arr, n):
     if n <= 1:
         return arr
@@ -61,3 +99,8 @@ assert sol_23([], 0) == []
 assert sol_23([1, 0, 2], 3) == [0, 1, 2]
 assert sol_23([2, 0, 1], 3) == [0, 1, 2]
 assert sol_23([2, 0, 1, 2, 0, 1, 1], 7) == [0, 0, 1, 1, 1, 2, 2]
+
+assert sol_23_v2([], 0) == []
+assert sol_23_v2([1, 0, 2], 3) == [0, 1, 2]
+assert sol_23_v2([2, 0, 1], 3) == [0, 1, 2]
+assert sol_23_v2([2, 0, 1, 2, 0, 1, 1], 7) == [0, 0, 1, 1, 1, 2, 2]
