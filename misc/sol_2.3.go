83c97590993d93d90d89521adfa782ecb89c16c4
Toran Sahu <toran.sahu@yahoo.com>
2022-11-10 17:19:33 +0530

    Sol 2.3

---
+++ misc/sol_2.3.go
@@ -0,0 +1,18 @@
+package misc
+
+import (
+	"fmt"
+	"strings"
+)
+
+// erase_kth_char(word String)
+func Sol2_3(word string, k int) string {
+	result := []string{}
+	for i, rune := range word {
+		char := fmt.Sprintf("%c", rune)
+		if (i+1)%k != 0 {
+			result = append(result, char)
+		}
+	}
+	return strings.Join(result, "")
+}
